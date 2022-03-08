c70c0e878847d1570d1808aebbc6f603dc4ae379
Tahmid Farhan <tahmidfarhan12@gmail.com>
2022-03-08 22:46:14 +0600

    port and release mode

--- main.go
+++ main.go
@@ -1,81 +1,16 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"goecom1/configs"
 	"goecom1/models/product"
-	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Current struct {
-	ID string `json:"_id"`
-	product.Product
-}
-
-func enterData() {
-	filename := "data.json"
-	plan, _ := ioutil.ReadFile(filename)
-	var data []Current
-	json.Unmarshal(plan, &data)
-
-	fmt.Println(data)
-
-	newData := []product.Product{}
-	allData := []interface{}{}
-
-	for _, item := range data {
-		newProduct := product.Product{}
-
-		newProduct.ID = primitive.NewObjectID()
-
-		newProduct.Description = item.Description
-		newProduct.Picture = item.Picture
-		newProduct.Price = item.Price
-		newProduct.Stock = item.Stock
-		newProduct.Title = item.Title
-
-		newData = append(newData, newProduct)
-
-		newDoc := bson.M{
-			"_id":         newProduct.ID,
-			"title":       newProduct.Title,
-			"description": newProduct.Description,
-			"picture":     newProduct.Picture,
-			"price":       newProduct.Price,
-			"stock":       newProduct.Stock,
-		}
-
-		allData = append(allData, newDoc)
-
-		fmt.Println(newProduct.ID)
-	}
-
-	file, _ := json.MarshalIndent(newData, "", " ")
-
-	_ = ioutil.WriteFile("newData.json", file, 0644)
-
-	products := configs.GetCollection("products")
-
-	result, err := products.InsertMany(context.TODO(), allData)
-
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println(result)
-
-}
-
 func main() {
-	// gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	router.SetTrustedProxies(nil)
 
