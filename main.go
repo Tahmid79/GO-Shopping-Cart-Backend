e882e96fd0d1ae05d1caf6f045c155ac37b41d0c
Tahmid Farhan <tahmidfarhan12@gmail.com>
2022-03-08 16:00:09 +0600

    remove ip

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"goecom1/configs"
 	"goecom1/models/product"
+	"log"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,7 +13,12 @@ func main() {
 	router := echo.New()
 	configs.ConnectDB()
 	AddRoutes(router)
-	router.Logger.Fatal(router.Start("localhost:3000"))
+	// router.Logger.Fatal(router.Start("localhost:3000"))
+
+	if err := router.Start(":3000"); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+
 }
 
 func AddRoutes(router *echo.Echo) {
